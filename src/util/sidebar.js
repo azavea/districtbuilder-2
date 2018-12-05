98c76344486913a44780eafec9edb8534d8a2946
jfrankl <jfrankl@azavea.com>
2018-12-04 21:53:12 -0500

    Add comment

--- src/util/sidebar.js
+++ src/util/sidebar.js
@@ -19,14 +19,15 @@ export const calculatePopulationsNew = (
 	geometries,
 	oldDistricts
 ) => {
-	console.log('Calculate Population Change');
+	// TODO: consider splitting out calculatePopulationsNew into two functions, which would allow us
+	// to separately memoize the selectedIds and the highlightedIds; not sure if this is a good idea
 	const newDistricts = JSON.parse(JSON.stringify(oldDistricts));
 	const allHighlightedIds = [...new Set([...selectedIds, ...activatedIds])];
 	allHighlightedIds.forEach(id => {
-		const unit = geometries[id];
+		const geounit = geometries[id];
 		populationTypes.forEach(type => {
-			newDistricts[selectedDistrict][type] += unit[type];
-			newDistricts[assignedDistricts[unit.id]][type] -= unit[type];
+			newDistricts[selectedDistrict][type] += geounit[type];
+			newDistricts[assignedDistricts[geounit.id]][type] -= geounit[type];
 		});
 	});
 	return newDistricts;
