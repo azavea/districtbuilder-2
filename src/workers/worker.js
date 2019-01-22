aff1682a06e0998960cd52d061d7edf973dcafb8
Jeff Frankl <jfrankl@azavea.com>
2019-01-22 09:52:38 -0500

    Remove local storage
    I think having local storage is going to make app development confusing. We are planning to remove this once we get the backend working.

--- src/workers/worker.js
+++ src/workers/worker.js
@@ -4,18 +4,17 @@ import Pbf from 'pbf';
 import { topoUrl } from '../constants';
 import { updateHighlight, getDistricts } from '../util/map';
 
-const topoRequest = fetch(topoUrl).then(res => res.json());
 let topoJSON;
 let type;
 let results;
 let districtCompactnessScores;
 let mergedGeoJSON;
 
-const promise = Promise.all([topoRequest]);
-
-promise.then(responses => {
-	topoJSON = responses[0];
-});
+fetch(topoUrl)
+	.then(res => res.json())
+	.then(response => {
+		topoJSON = response;
+	});
 
 const onDistrictDraw = (e, results) => {
 	type = e.data.type;
@@ -26,35 +25,33 @@ const onDistrictDraw = (e, results) => {
 };
 
 onmessage = function(e) {
-	switch (e.data.type) {
-		case 'DISTRICTS':
-			if (topoJSON) {
+	if (topoJSON) {
+		switch (e.data.type) {
+			case 'DISTRICTS':
 				onDistrictDraw(e, results);
-			} else {
-				promise.then(onDistrictDraw(e, results));
-			}
-			break;
-		case 'DOWNLOAD_GEOJSON':
-			type = e.data.type;
-			results = getDistricts(e.data.assignedDistricts, topoJSON);
-			mergedGeoJSON = geobuf.encode(results.mergedGeoJSON, new Pbf()).buffer;
-			postMessage({ type, mergedGeoJSON }, [mergedGeoJSON]);
-			break;
-		case 'HIGHLIGHT':
-			type = e.data.type;
-			results = geobuf.encode(
-				updateHighlight(e.data.selectedIds, e.data.activatedIds, topoJSON),
-				new Pbf()
-			).buffer;
-			postMessage(
-				{
-					type,
-					results,
-				},
-				[results]
-			);
-			break;
-		default:
-			break;
+				break;
+			case 'DOWNLOAD_GEOJSON':
+				type = e.data.type;
+				results = getDistricts(e.data.assignedDistricts, topoJSON);
+				mergedGeoJSON = geobuf.encode(results.mergedGeoJSON, new Pbf()).buffer;
+				postMessage({ type, mergedGeoJSON }, [mergedGeoJSON]);
+				break;
+			case 'HIGHLIGHT':
+				type = e.data.type;
+				results = geobuf.encode(
+					updateHighlight(e.data.selectedIds, e.data.activatedIds, topoJSON),
+					new Pbf()
+				).buffer;
+				postMessage(
+					{
+						type,
+						results,
+					},
+					[results]
+				);
+				break;
+			default:
+				break;
+		}
 	}
 };
