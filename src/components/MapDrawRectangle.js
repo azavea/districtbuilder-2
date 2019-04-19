b1234d777ebeee1353551bce1d17de117c2516fb
jfrankl <jfrankl@azavea.com>
2019-04-19 16:59:37 -0400

    WIP

--- src/components/MapDrawRectangle.js
+++ src/components/MapDrawRectangle.js
@@ -10,98 +10,103 @@ import { withMap } from './Context';
 import { geounitLayerName } from '../constants';
 
 class MapDrawHandler extends Component {
-  hovered = [];
-  componentDidMount() {
-    this.limitDrawFilter = (drawLimit, rectangleStartId, countyFps) => {
-      return drawLimit ? rectangleStartId === countyFps : true;
-    };
+  active = [];
+  mouseDown = true;
+  limitDrawFilter = (drawLimit, rectangleStartId, countyFps) => {
+    return drawLimit ? rectangleStartId === countyFps : true;
+  };
 
-    this.lockedFilter = (lockedIds, districts, id) => {
-      return !lockedIds[districts[id]];
-    };
+  lockedFilter = (lockedIds, districts, id) => {
+    return !lockedIds[districts[id]];
+  };
 
-    this.lockedFilter = (lockedIds, districts, id) => {
-      return !lockedIds[districts[id]];
-    };
+  lockedFilter = (lockedIds, districts, id) => {
+    return !lockedIds[districts[id]];
+  };
 
-    this.selectedFilter = (selectedDistrict, districts, id) => {
-      return id !== selectedDistrict;
-    };
+  selectedFilter = (selectedDistrict, districts, id) => {
+    return id !== selectedDistrict;
+  };
 
-    this.removeHover = () => {
-      this.hovered.forEach(feature => {
-        this.props.map.setFeatureState(
-          {
-            source: 'blockgroups',
-            sourceLayer: geounitLayerName,
-            id: feature.id,
-          },
-          { hover: false }
-        );
-      });
-    };
+  removeHover = () => {
+    this.active.forEach(feature => {
+      this.props.map.setFeatureState(
+        {
+          source: 'blockgroups',
+          sourceLayer: geounitLayerName,
+          id: feature.id,
+        },
+        { hover: false }
+      );
+    });
+  };
 
-    this.onRectangleActivate = (bbox, action) => {
-      const { lockedIds, districts, map, selectionLevel, selectedDistrict } = this.props;
-      const southWest = [bbox[0], bbox[1]];
-      const northEast = [bbox[2], bbox[3]];
-      const southWestPointPixel = this.props.map.project(southWest);
-      const northEastPointPixel = this.props.map.project(northEast);
+  onRectangleActivate = (bbox, action) => {
+    const { lockedIds, districts, map, selectionLevel, selectedDistrict } = this.props;
+    const southWest = [bbox[0], bbox[1]];
+    const northEast = [bbox[2], bbox[3]];
+    const southWestPointPixel = this.props.map.project(southWest);
+    const northEastPointPixel = this.props.map.project(northEast);
 
-      switch (selectionLevel) {
-        case 'geounit':
-          this.removeHover();
-          this.hovered = map
-            .queryRenderedFeatures([southWestPointPixel, northEastPointPixel], {
-              layers: ['geounits-fill'],
-            })
-            .filter(feature => {
-              return (
-                // this.selectedFilter(selectedDistrict, districts, feature.properties.id) &&
-                this.lockedFilter(lockedIds, districts, feature.properties.id)
+    switch (selectionLevel) {
+      case 'geounit':
+        // if (!this.mouseDown) {
+        this.removeHover();
+        // }
+        const hovered = map
+          .queryRenderedFeatures([southWestPointPixel, northEastPointPixel], {
+            layers: ['geounits-fill'],
+          })
+          .filter(feature => {
+            return (
+              // this.selectedFilter(selectedDistrict, districts, feature.properties.id) &&
+              this.lockedFilter(lockedIds, districts, feature.properties.id)
+            );
+          });
+        this.active = [...hovered, ...this.active];
+        // Is it necessary to use the ...new Set opperation? Might be able to remove
+        action([
+          ...new Set(
+            this.active.map(feature => {
+              this.props.map.setFeatureState(
+                {
+                  source: 'blockgroups',
+                  sourceLayer: geounitLayerName,
+                  id: feature.id,
+                },
+                { hover: true }
               );
-            });
-          // Is it necessary to use the ...new Set opperation? Might be able to remove
-          action([
-            ...new Set(
-              this.hovered.map(feature => {
-                this.props.map.setFeatureState(
-                  {
-                    source: 'blockgroups',
-                    sourceLayer: geounitLayerName,
-                    id: feature.id,
-                  },
-                  { hover: true }
-                );
-                return feature.properties.id;
-              })
-            ),
-          ]);
-          break;
-        case 'county':
-          this.removeHover();
-          action([
-            ...new Set(
-              flat(
-                map
-                  .queryRenderedFeatures([southWestPointPixel, northEastPointPixel], {
-                    layers: ['counties-fill'],
-                  })
-                  .map(feature => window.dataCountyIndex[feature.properties.countyfp].geounits)
-              ).filter(id => {
-                return this.lockedFilter(lockedIds, districts, id);
-              })
-            ),
-          ]);
-          break;
-        default:
-          break;
-      }
-    };
+              return feature.properties.id;
+            })
+          ),
+        ]);
+        break;
+      case 'county':
+        this.removeHover();
+        action([
+          ...new Set(
+            flat(
+              map
+                .queryRenderedFeatures([southWestPointPixel, northEastPointPixel], {
+                  layers: ['counties-fill'],
+                })
+                .map(feature => window.dataCountyIndex[feature.properties.countyfp].geounits)
+            ).filter(id => {
+              return this.lockedFilter(lockedIds, districts, id);
+            })
+          ),
+        ]);
+        break;
+      default:
+        break;
+    }
+  };
 
-    this.onRectangleActivateDebounced = debounce(this.onRectangleActivate, 200, { maxWait: 500 });
-    this.onPaintActivateThrottled = throttle(this.onRectangleActivate, 50, { maxWait: 50 });
+  onRectangleActivateDebounced = debounce(this.onRectangleActivate, 200, { maxWait: 500 });
+  onPaintActivateThrottled = throttle(this.onRectangleActivate, 500);
 
+  componentDidMount() {
+    this.props.map.dragPan.disable();
     this.props.map.on('draw.create', e => {
       this.onRectangleActivate(bbox(e.features[0]), this.props.onSelectResults);
       this.props.draw.deleteAll();
@@ -116,10 +121,16 @@ class MapDrawHandler extends Component {
 
     this.props.map.on('mousemove', e => {
       const { lng, lat } = e.lngLat;
-      const brushSize = 0.05;
+      const brushSize = 0.005;
       const paintBbox = [lng - brushSize, lat - brushSize, lng + brushSize, lat + brushSize];
       this.onPaintActivateThrottled(paintBbox, this.props.onActivateResults);
     });
+    document.body.onmousedown = () => {
+      this.mouseDown = true;
+    };
+    document.body.onmouseup = () => {
+      this.mouseDown = false;
+    };
   }
   render() {
     return <div className="map-draw-rectangle" />;
