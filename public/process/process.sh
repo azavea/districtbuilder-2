af4a3fd362d52a704656a88fdd9fb45d3bbe0159
Tyler Gilcrest <tgilcrest@azavea.com>
2020-05-23 17:38:19 -0400

    Update process.sh to take params and lookup state

--- public/process/process.sh
+++ public/process/process.sh
@@ -1,48 +1,115 @@
-rm -rf _output
+#!/bin/bash
 
-mkdir _output _output/county _output/bg _output/precinct _output/location _output/mbtiles _output/upload
+set -e
+
+error_exit()
+{
+	echo "$1" 1>&2
+	exit 1
+}
+
+usage()
+{
+    echo "usage: process.sh [[[-s state ] [-d input_directory]] | [-h]]"
+}
+
+lookup=./state-lookup.conf
+
+if [[ ! -f "$lookup" ]]; then
+    error_exit "Missing state lookup config file. Exiting."
+fi
+
+. "$lookup"
+
+while getopts s:d:h flag
+do
+    case "${flag}" in
+        s) state=${OPTARG};;
+        d) input_directory=${OPTARG};;
+        h) usage; exit;;
+    esac
+done
+
+if [[ ! "$state" || ! "$input_directory" ]]; then
+    error_exit "State and input directory must be specified. Use -h to see usage."
+fi
+
+if [[ ${!state} == "" ]]; then
+    error_exit "Error: State not found. Check state-lookup.conf for available states."
+fi
+
+out_dir=./_output/"$state"
+
+if [[ -d "$out_dir" ]]; then
+    echo "Data for state already exists. Overwrite? (y/n)"
+        while : ; do
+            read reply
+            if [[ $reply == "y" ]]; then
+                echo "Removing existing state data."
+                rm -rf "$out_dir"
+                break
+            elif [[ $reply == "n" ]]; then
+                error_exit "Existing state data was not overwritten. Exiting."
+            else
+                echo "Unexpected input. Overwrite? (y/n)"
+            fi
+        done
+fi
+echo "Creating output directory."
+
+county_dir="$out_dir"/county
+bg_dir="$out_dir"/bg
+precinct_dir="$out_dir"/precinct
+location_dir="$out_dir"/location
+mbtiles_dir="$out_dir"/mbtiles
+upload_dir="$out_dir"/upload
+
+
+mkdir -p "$out_dir" "$county_dir" "$bg_dir" "$precinct_dir" "$location_dir" "$mbtiles_dir" "$upload_dir"
 
 # Export to GeoJSON
-mapshaper -i input/pa-bg.geojson -o _output/bg/bg-lines.geojson format=geojson
-mapshaper -i input/pa-counties.geojson -o _output/county/county-lines.geojson format=geojson
+echo "Exporting source files to geojson."
+mapshaper -i "$input_directory"/bg/*.geojson -o "$bg_dir"/bg-lines.geojson format=geojson
+mapshaper -i "$input_directory"/c/*.geojson -o "$county_dir"/county-lines.geojson format=geojson
 
-node generateDataFeaturesJson.js -i _output/bg/bg-lines.geojson -o _output/upload/bg-features.json
-node generateCountyIndex.js -c _output/county/county-lines.geojson -g _output/bg/bg-lines.geojson -o _output/upload/county-index.json
-node generateAssignedDistricts.js -i _output/bg/bg-lines.geojson -o _output/upload/assigned-districts.json
-node abbreviatePopulationValues.js -i _output/bg/bg-lines.geojson -o _output/bg/bg-lines.geojson
-node abbreviatePopulationValues.js -i _output/county/county-lines.geojson -o _output/county/county-lines.geojson
+node generateDataFeaturesJson.js -i "$bg_dir"/bg-lines.geojson -o "$out_dir"/upload/bg-features.json
+node generateCountyIndex.js -c "$county_dir"/county-lines.geojson -g "$bg_dir"/bg-lines.geojson -o "$out_dir"/upload/county-index.json
+node generateAssignedDistricts.js -i "$bg_dir"/bg-lines.geojson -o "$out_dir"/upload/assigned-districts.json
+node abbreviatePopulationValues.js -i "$bg_dir"/bg-lines.geojson -o "$bg_dir"/bg-lines.geojson
+node abbreviatePopulationValues.js -i "$county_dir"/county-lines.geojson -o "$county_dir"/county-lines.geojson
 
-# Filter location labels to county
-mapshaper -i input/city-l.geojson -filter 'statefp==="42"' -o _output/location/city-l.geojson format=geojson
-mapshaper -i input/city-m.geojson -filter 'statefp==="42"' -o _output/location/city-m.geojson format=geojson
-mapshaper -i input/city-s.geojson -filter 'statefp==="42"' -o _output/location/city-s.geojson format=geojson
-mapshaper -i input/town-l.geojson -filter 'statefp==="42"' -o _output/location/town-l.geojson format=geojson
-mapshaper -i input/town-m.geojson -filter 'statefp==="42"' -o _output/location/town-m.geojson format=geojson
-mapshaper -i input/town-s.geojson -filter 'statefp==="42"' -o _output/location/town-s.geojson format=geojson
+# Filter location labels to state
+echo "Filtering location labels to state."
+for f in ./input/*.geojson; do
+    mapshaper -i "$f" -filter \'statefp===\"${!state}\"\' -o "$location_dir"/"$(basename "$f")" format=geojson
+done
 
 # Generate Labels
-geojson-polygon-labels _output/bg/bg-lines.geojson > _output/bg/bg-labels.geojson
-geojson-polygon-labels _output/county/county-lines.geojson > _output/county/county-labels.geojson
+echo "Generating labels for polygons."
+geojson-polygon-labels "$bg_dir"/bg-lines.geojson > "$bg_dir"/bg-labels.geojson
+geojson-polygon-labels "$county_dir"/county-lines.geojson > "$county_dir"/county-labels.geojson
 
 # Export to TopoJSON
-mapshaper -i _output/bg/bg-lines.geojson -simplify 0.4 -o _output/bg/bg-lines.topojson format=topojson drop-table
+echo "Exporting geojson to topojson."
+mapshaper -i "$bg_dir"/bg-lines.geojson -simplify 0.4 -o "$bg_dir"/bg-lines.topojson format=topojson drop-table
 
 # Generate vector tiles
-tippecanoe -f -pk --no-tile-compression -o _output/mbtiles/bg.mbtiles --simplification=15 --generate-ids --detect-shared-borders --maximum-zoom=10 --minimum-zoom=4 --simplify-only-low-zooms --no-tiny-polygon-reduction _output/bg/bg-lines.geojson
-tippecanoe -f -pk --no-tile-compression -o _output/mbtiles/county.mbtiles --simplification=15 --generate-ids --detect-shared-borders --maximum-zoom=10 --minimum-zoom=4 --simplify-only-low-zooms --no-tiny-polygon-reduction _output/county/county-lines.geojson
+echo "Generating vector tiles."
+tippecanoe -f -pk --no-tile-compression -o "$mbtiles_dir"/bg.mbtiles --simplification=15 --generate-ids --detect-shared-borders --maximum-zoom=10 --minimum-zoom=4 --simplify-only-low-zooms --no-tiny-polygon-reduction "$bg_dir"/bg-lines.geojson
+tippecanoe -f -pk --no-tile-compression -o "$mbtiles_dir"/county.mbtiles --simplification=15 --generate-ids --detect-shared-borders --maximum-zoom=10 --minimum-zoom=4 --simplify-only-low-zooms --no-tiny-polygon-reduction "$county_dir"/county-lines.geojson
 
-tippecanoe -f -pk --no-tile-compression -o _output/mbtiles/bg-labels.mbtiles --maximum-zoom=10 --minimum-zoom=4 -r1 _output/bg/bg-labels.geojson
-tippecanoe -f -pk --no-tile-compression -o _output/mbtiles/county-labels.mbtiles --maximum-zoom=10 --minimum-zoom=4 -r1 _output/county/county-labels.geojson
+tippecanoe -f -pk --no-tile-compression -o "$mbtiles_dir"/bg-labels.mbtiles --maximum-zoom=10 --minimum-zoom=4 -r1 "$bg_dir"/bg-labels.geojson
+tippecanoe -f -pk --no-tile-compression -o "$mbtiles_dir"/county-labels.mbtiles --maximum-zoom=10 --minimum-zoom=4 -r1 "$county_dir"/county-labels.geojson
 
-tippecanoe -f -pk --no-tile-compression -o _output/mbtiles/city-l.mbtiles --maximum-zoom=10 --minimum-zoom=4  _output/location/city-l.geojson
-tippecanoe -f -pk --no-tile-compression -o _output/mbtiles/city-m.mbtiles --maximum-zoom=10 --minimum-zoom=4  _output/location/city-m.geojson
-tippecanoe -f -pk --no-tile-compression -o _output/mbtiles/city-s.mbtiles --maximum-zoom=10 --minimum-zoom=4  _output/location/city-s.geojson
-tippecanoe -f -pk --no-tile-compression -o _output/mbtiles/town-l.mbtiles --maximum-zoom=10 --minimum-zoom=7 --drop-densest-as-needed  _output/location/town-l.geojson
-tippecanoe -f -pk --no-tile-compression -o _output/mbtiles/town-m.mbtiles --maximum-zoom=10 --minimum-zoom=7 --drop-densest-as-needed  _output/location/town-m.geojson
-tippecanoe -f -pk --no-tile-compression -o _output/mbtiles/town-s.mbtiles --maximum-zoom=10 --minimum-zoom=8 --drop-densest-as-needed  _output/location/town-s.geojson
+tippecanoe -f -pk --no-tile-compression -o "$mbtiles_dir"/city-l.mbtiles --maximum-zoom=10 --minimum-zoom=4  "$location_dir"/city-l.geojson
+tippecanoe -f -pk --no-tile-compression -o "$mbtiles_dir"/city-m.mbtiles --maximum-zoom=10 --minimum-zoom=4  "$location_dir"/city-m.geojson
+tippecanoe -f -pk --no-tile-compression -o "$mbtiles_dir"/city-s.mbtiles --maximum-zoom=10 --minimum-zoom=4  "$location_dir"/city-s.geojson
+tippecanoe -f -pk --no-tile-compression -o "$mbtiles_dir"/town-l.mbtiles --maximum-zoom=10 --minimum-zoom=7 --drop-densest-as-needed  "$location_dir"/town-l.geojson
+tippecanoe -f -pk --no-tile-compression -o "$mbtiles_dir"/town-m.mbtiles --maximum-zoom=10 --minimum-zoom=7 --drop-densest-as-needed  "$location_dir"/town-m.geojson
+tippecanoe -f -pk --no-tile-compression -o "$mbtiles_dir"/town-s.mbtiles --maximum-zoom=10 --minimum-zoom=8 --drop-densest-as-needed  "$location_dir"/town-s.geojson
 
-tile-join -pk --no-tile-compression -o _output/mbtiles/combined.mbtiles _output/mbtiles/bg.mbtiles _output/mbtiles/county.mbtiles _output/mbtiles/city-l.mbtiles _output/mbtiles/city-m.mbtiles _output/mbtiles/city-s.mbtiles _output/mbtiles/town-l.mbtiles _output/mbtiles/town-m.mbtiles _output/mbtiles/town-s.mbtiles _output/mbtiles/bg-labels.mbtiles _output/mbtiles/county-labels.mbtiles
+tile-join -pk --no-tile-compression -o "$mbtiles_dir"/combined.mbtiles "$mbtiles_dir"/bg.mbtiles "$mbtiles_dir"/county.mbtiles "$mbtiles_dir"/city-l.mbtiles "$mbtiles_dir"/city-m.mbtiles "$mbtiles_dir"/city-s.mbtiles "$mbtiles_dir"/town-l.mbtiles "$mbtiles_dir"/town-m.mbtiles "$mbtiles_dir"/town-s.mbtiles "$mbtiles_dir"/bg-labels.mbtiles "$mbtiles_dir"/county-labels.mbtiles
 
-mb-util --image_format=pbf _output/mbtiles/combined.mbtiles _output/upload/tiles
+mb-util --image_format=pbf "$mbtiles_dir"/combined.mbtiles "$upload_dir"/tiles
 
-cp _output/bg/bg-lines.topojson _output/upload/bg-lines.topojson
+cp "$bg_dir"/bg-lines.topojson "$upload_dir"/bg-lines.topojson
